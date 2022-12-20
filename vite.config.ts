93ad03f72b22f15f35bcff3dd010f3fa5dd0dd9d
Max Zakharzhevskiy <maxzz@msn.com>
2022-12-19 16:16:41 -0800

    WIP. nunjunks plugin. nunjucks is not a function error

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,22 @@
 import path from 'path';
 import { defineConfig } from 'vite';
+import {default as nunjucks} from 'vite-plugin-nunjucks';
+
+console.log('nunjucks', nunjucks);
+nunjucks()
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    base: '',
-    plugins: [
-    ],
-    server: {
-        port: 3000,
-    },
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src'),
-        }
-    }
-});
+// export default defineConfig({
+//     base: '',
+//     plugins: [
+//         nunjucks(),
+//     ],
+//     server: {
+//         port: 3000,
+//     },
+//     resolve: {
+//         alias: {
+//             '@': path.resolve(__dirname, './src'),
+//         }
+//     }
+// });
