851d29b488ed3ee3ca7fa945016facdf2725b379
Max Zakharzhevskiy <maxzz@msn.com>
2022-12-19 16:58:44 -0800

    WIP. post html. remove @vituum/posthtml; it's still vituum plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,11 @@
 import path from 'path';
 import { defineConfig } from 'vite';
-import posthtml from '@vituum/posthtml';
 
 //https://vitejs.dev/config/
 export default defineConfig({
     base: '',
     plugins: [
-        posthtml(),
     ],
-    // integrations: [posthtml()],
-
     server: {
         port: 3000,
     },
