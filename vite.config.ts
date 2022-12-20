ac864ff1c5e615fec259af8710de2bc6e913f3e6
Max Zakharzhevskiy <maxzz@msn.com>
2022-12-19 17:14:46 -0800

    WIP. post html. add vite-plugin-html-inject

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import path from 'path';
 import { defineConfig } from 'vite';
+import htmlInject from 'vite-plugin-html-inject';
 
 //https://vitejs.dev/config/
 export default defineConfig({
     base: '',
     plugins: [
+        htmlInject(),
     ],
     server: {
         port: 3000,
