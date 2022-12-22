78c450a97fdf450b39a4f8fba1582dbb7cd26c51
Max Zakharzhevskiy <maxzz@msn.com>
2022-12-21 17:08:13 -0800

    WIP. html. clean up css

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', '.src/**/*.{html,ts,js}'],
+  content: ['./index.html', './src/**/*.{html,ts,js}'],
   theme: {
     extend: {},
   },
