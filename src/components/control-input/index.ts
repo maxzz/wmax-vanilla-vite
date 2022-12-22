e407ad2803c9145298af2a715e201b009458bbcb
Max Zakharzhevskiy <maxzz@msn.com>
2022-12-21 16:33:51 -0800

    WIP. html. add elm. w/ innerHTML 1st child can be comment

--- src/components/control-input/index.ts
+++ src/components/control-input/index.ts
@@ -4,10 +4,10 @@ export function createInput() {
     const el = document.createElement('div');
     el.innerHTML = layout;
 
-    if (el.children.length > 1) {
-        return el.children;
-    }
-    
+    // if (el.children.length > 1) {
+    //     return el.children;
+    // }
+
     if (!el.firstChild) {
         throw new Error('bad template');
     }
