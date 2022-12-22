f7e4d9fa91d12cea7c12b70e498ac3377a191ea9
Max Zakharzhevskiy <maxzz@msn.com>
2022-12-21 18:25:08 -0800

    WIP. html. diff. grid layout. 5. add more controls

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './src/**/*.{html,ts,js}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ['./index.html', './src/**/*.{html,ts,js}'],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        require('@tailwindcss/forms')({
+            strategy: 'base', // only generate global styles
+            //strategy: 'class', // only generate classes
+        }),
+    ],
+};
