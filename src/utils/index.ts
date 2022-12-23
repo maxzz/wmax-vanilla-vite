224d9010af3953994809c9ec3950f3210bf20192
Max Zakharzhevskiy <maxzz@msn.com>
2022-12-22 16:46:34 -0800

    WIP. dynamic form. 4. update util functions

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,22 +1,16 @@
-export function elementFromString<T extends HTMLElement = HTMLElement>(layout: string): T {
-    const el = document.createElement('div');
-    el.innerHTML = layout;
-    if (!el.firstChild) {
-        throw new Error('bad template');
-    }
-    return el.firstChild as T;
-}
-
-export function controlFromLayout(str: string): HTMLElement {
+export function textToElement<T extends HTMLElement = HTMLElement>(str: string): T {
     const el = document.createElement('div');
     el.innerHTML = str;
-    return el.firstElementChild as HTMLElement;
+    if (!el.firstChild) {
+        throw new Error(`Bad template: ${str}`);
+    }
+    return el.firstElementChild as T;
 }
 
 export function query<T extends Element = HTMLElement>(selector: string, parent: Element | Document = document): T {
     const el = parent.querySelector<T>(selector);
     if (!el) {
-        throw new Error(`cannot get element by '${selector}' selector`);
+        throw new Error(`Failed to get element by '${selector}' selector.`);
     }
     return el;
 }
