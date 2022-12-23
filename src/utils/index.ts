ff90df11bc43f44a0016db1d6f1ac27984fa1a95
Max Zakharzhevskiy <maxzz@msn.com>
2022-12-22 16:41:02 -0800

    WIP. dynamic form. 3. add query()

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -11,4 +11,12 @@ export function controlFromLayout(str: string): HTMLElement {
     const el = document.createElement('div');
     el.innerHTML = str;
     return el.firstElementChild as HTMLElement;
-} 
+}
+
+export function query<T extends Element = HTMLElement>(selector: string, parent: Element | Document = document): T {
+    const el = parent.querySelector<T>(selector);
+    if (!el) {
+        throw new Error(`cannot get element by '${selector}' selector`);
+    }
+    return el;
+}
